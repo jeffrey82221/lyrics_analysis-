c8b95ffcdc330d4d78d38ab4ecc6c579c4aa8af1
Jeffrey Lin <jeffrey82221@gmail.com>
2016-04-25 17:44:27 +0800

    change to  pool async

--- ReadInfo.py
+++ ReadInfo.py
@@ -294,7 +294,8 @@ class LyricsData:
 
             multi_lyricsinfos = self.splitarray(self.lyricsinfos,multiprocessing.cpu_count())
             pool = multiprocessing.Pool(multiprocessing.cpu_count())
-            multi_voc_set = pool.map(generate_voc_set, multi_lyricsinfos)
+            #multi_voc_set = pool.map(generate_voc_set, multi_lyricsinfos)
+            multi_voc_set=[ pool.apply_async(generate_voc_set, (multi_lyricsinfos[i],)) for i in range(len(multi_lyricsinfos)) ]
             voc_set = set().union(*multi_voc_set)
             voc_array = list(voc_set)
             voc_array.sort()
